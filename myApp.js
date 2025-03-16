40c2aa494f7c64cf1d9d64173e6eb8e233e14331
em-latic <emanuel.munoz-orlich@outlook.com>
2025-03-16 14:26:48 -0600

    Helmet config done

--- myApp.js
+++ myApp.js
@@ -1,6 +1,40 @@
 const express = require('express');
+const helmet = require('helmet');
+
 const app = express();
 
+// app.use(helmet.hidePoweredBy());
+// app.use(helmet.frameguard({action: 'deny'}));
+// app.use(helmet.xssFilter());
+// app.use(helmet.noSniff());
+// app.use(helmet.ieNoOpen());
+
+const ninetyDaysInSec = 90*24*60*60;
+// app.use(helmet.hsts({maxAge:ninetyDaysInSec, force: true}));
+// app.use(helmet.dnsPrefetchControl());
+// app.use(helmet.noCache());
+
+// const directives = {defaultSrc: ["'self'"], scriptSrc: ["'self'", 'trusted-cdn.com']};
+// app.use(helmet.contentSecurityPolicy({ directives: directives }));
+
+app.use(
+  helmet({
+    hidePoweredBy: true,
+    frameguard: { action: 'deny' },
+    xssFilter: true,
+    noSniff: true,
+    ieNoOpen: true,
+    hsts: { maxAge: ninetyDaysInSec, force: true },
+    dnsPrefetchControl: true,
+    noCache: true,
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", 'trusted-cdn.com']
+      }
+    }
+  })
+);
 
 
 
